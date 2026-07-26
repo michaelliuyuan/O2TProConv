92d97649fde9c264c07665daef4598b2176c217a
michael_ly <michael_ly@users.noreply.github.com>
2026-07-26 10:45:07 +0800

    fix(convert): strip UTF-8 BOM in convert_one to prevent header parse issues

--- lib/convert.sh
+++ lib/convert.sh
@@ -665,6 +665,7 @@ _strip_gbk_comments() {
 convert_one() {
   local in="$1" out="$2"
   local text; text="$(cat "$in")"
+  text="$(sed -E '1s/^\xEF\xBB\xBF//' <<<"$text")"
   text="$(_tochar_date       <<<"$text")"
   text="$(_strip_gbk_comments <<<"$text")"
   text="$(_resolve_anchor_type <<<"$text")"
